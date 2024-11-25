9efbfed10bb2a54483d32db2c0d12df04289696a
Andrew Valencik <andrew.valencik@shopify.com>
2024-11-25 18:19:43 -0500

    Tweak comments and naming in associateOps

--- core/src/main/scala/pink/cozydev/lucille/internal/Op.scala
+++ core/src/main/scala/pink/cozydev/lucille/internal/Op.scala
@@ -38,42 +38,45 @@ private[lucille] object Op {
         var currentOp = headOp
         var currentQ = headQ
 
-        // We'll collect queries in 'tempAccumulator' while successive OPs are the same type
+        // We'll collect queries in 'tempAccumulator' while successive operators are the same type
+        // e.g. (OR, q1), (OR, q2), (OR, q3), ...
         val tempAccumulator = ListBuffer.empty[Query]
         tempAccumulator += first
 
-        // When successive OPs change type, we clear 'tempAccumulator' and add them to 'outerBlder'
-        val outerBlder = ListBuffer.empty[Query]
+        // When successive operators change type, we clear 'tempAccumulator' and add them to 'bldr'
+        // e.g. (OR, q1), (AND, q2), ...
+        val bldr = ListBuffer.empty[Query]
 
-        // Iterate through OP-Query pairs, looking "one ahead" to deside how to process 'currentQ'
+        // Iterate through Op-Query pairs, looking "one ahead" to decide how to process 'currentQ'
         remaining.foreach { case (nextOp, nextQ) =>
           if (currentOp == nextOp) {
-            // nextOp hasn't changed, keep accumulating
+            // 'nextOp' hasn't changed, keep accumulating
             tempAccumulator += currentQ
           } else {
             // 'nextOp' is different from 'currentOp', so we're going to collapse the queries we've
             // accumulated so far into an AND/OR query before continuing.
-            // How we do that depends on the precedence of the operator we're switching to
-            // if we are switching to AND, it has higher precedence than OR, so we collapse before
-            // accumulating 'currentQ' and instead add it to the newly cleared accumulator.
+            // How we do that depends on the precedence of the operator we're switching to.
+            // AND has higher precedence than OR, so if we are switching from OR to AND, we
+            // collapse before accumulating 'currentQ' and instead add it to the newly cleared
+            // accumulator.
             nextOp match {
               case AND =>
                 // OR -> AND
-                // previousQ OR (currentQ AND nextQ)
+                // e.g. previousQ OR (currentQ AND nextQ)
                 // From OR to AND, collapse now, new AND gets currentQ
                 val qs = tempAccumulator.result()
                 tempAccumulator.clear()
-                outerBlder ++= qs
+                bldr ++= qs
                 tempAccumulator += currentQ
               case OR =>
                 // AND -> OR
-                // (previousQ AND currentQ) OR nextQ
+                // e.g. (previousQ AND currentQ) OR nextQ
                 // From AND to OR, add currentQ before collapsing
                 tempAccumulator += currentQ
                 val qs = tempAccumulator.result()
                 tempAccumulator.clear()
 
-                outerBlder += Query.And.fromListUnsafe(qs)
+                bldr += Query.And.fromListUnsafe(qs)
             }
           }
           // get ready for next iteration
@@ -82,22 +85,25 @@ private[lucille] object Op {
         }
 
         // We're done iterating
-        // But because we were looking one ahead, we still have not processed the last 'currentQ'
-        // Safe to add 'currentQ' to 'tempAccumulator', it's either collecting the same type of
-        // queries, or we've just emptied it for this new type of query.
+        // But because we were looking one ahead, we still have not processed the last 'currentQ'.
+        // It's safe to add 'currentQ' to 'tempAccumulator', it's either already collecting queries
+        // for 'currentOp', or we've just cleared it for a new Op type.
         tempAccumulator += currentQ
         val innerQs = tempAccumulator.result()
         currentOp match {
           case AND =>
-            // Final OP was an AND, collapse into one AND query, add to outer
-            outerBlder += Query.And.fromListUnsafe(innerQs)
+            // Final OP was an AND, collapse into one AND query, add to 'bldr'
+            bldr += Query.And.fromListUnsafe(innerQs)
           case OR =>
-            // Final OP was an OR, directly add to outer
-            outerBlder ++= innerQs
+            // Final OP was an OR, directly add to 'bldr'.
+            // Safe because all the ANDs have been grouped together.
+            // Wrapping in an OR query would create unnecessary nesting.
+            bldr ++= innerQs
         }
-        val outerQs = outerBlder.result()
-        // If we only have one query, directly return that, otherwise wrap in OR
-        if (outerQs.size == 1) outerQs.head else Query.Or.fromListUnsafe(outerQs)
+        val finalQs = bldr.result()
+        // If we only have one query, it must be an AND query, directly return that.
+        // Otherwise we wrap our multiple queries in an OR query.
+        if (finalQs.size == 1) finalQs.head else Query.Or.fromListUnsafe(finalQs)
     }
 
 }
